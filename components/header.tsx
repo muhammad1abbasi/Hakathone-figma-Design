66b3d642bc2dd9dd8ac20693156d3a4744806cf8
muhammad1abbasi <muhammadabbasi724@gmail.com>
2024-12-09 23:39:20 +0500

    message

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import React from 'react'
 
@@ -8,17 +9,42 @@ const Header = () => {
         <div className='flex justify-evenly items-center'>
             <div className='flex flex-col sm:flex-row sm:space-x-28 '>
             <Image src="/images/Hekto.svg" alt='igm' width={98} height={34}/>
-             <ul className='sm:flex space-x-1 sm:space-x-3 mt-3 hidden'>
+             {/* <ul className='sm:flex space-x-1 sm:space-x-3 mt-3 hidden'>
                 <li className='text-red-500 flex'>Home
                 <Image src="/images/redarrowdown.svg" alt='igm' width={12} height={12}/>
                 </li>
                 
-                <li>Pages</li>
+                <li> Pages</li>
                 <li>Products</li>
                 <li>Blog </li>
                 <li>Shop</li>
                 <li>Contact</li>
-             </ul>
+             </ul> */}
+
+
+<ul className='sm:flex space-x-1 sm:space-x-5 mt-3 hidden'>
+        <li>
+          <Link href="/gridpage">
+            Grid Products Page
+          
+          </Link>
+        </li>
+        <li>
+          <Link href="/Login">
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link href="/norend">
+            404 Trouble
+          </Link>
+        </li>
+        <li>
+          <Link href="/productDetails">
+            Products Details
+          </Link>
+        </li>
+      </ul>
 
             </div>
 
